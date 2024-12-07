fbc371b5d1ad4cdc94e34d0e20e7bd5c671602de
MARIA ASKITI <science.askiti@gmail.com>
2024-12-07 13:44:49 +0200

    form.js

--- form.js
+++ form.js
@@ -1,17 +1,17 @@
 <script>
-			window.onload=()=>
-			{
-				const form = document.querySelector("form");
-				const input = document.querySelector("#txtTel");
-				form.onsubmit = (e) =>
-				{
-					const phoneNumber = input.value.trim();
-					if (!/^\d{10}$/.test(phoneNumber))
-					{
-						e.preventDefault();
-						alert("Please enter a valid phone number.");
-			          }
-				};
+	window.onload=()=>
+	{
+		const form = document.querySelector("form");
+		const input = document.querySelector("#txtTel");
+		form.onsubmit = (e) =>
+	{
+		const phoneNumber = input.value.trim();
+		if (!/^\d{10}$/.test(phoneNumber))
+		{
+			e.preventDefault();
+			alert("Please enter a valid phone number.");
+		 }
+	};
 
-			};
-		</script>
+	};
+</script>
